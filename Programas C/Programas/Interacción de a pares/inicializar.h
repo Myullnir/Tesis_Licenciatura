6a3a329e176698ee7349133516dad12f6c386603
Favio Di Ciocco <42681829+Myullnir@users.noreply.github.com>
2021-01-07 23:13:24 -0300

    Interacción de a Pares
    En esta versión el programa lo que hace es recorrer todos los sujetos de la red de forma ordenada, pero al evaluar la evolución de sus tópicos lo que hace es conectar a cada agente con un ÚNICO agente ALEATORIO. Estas dos son las grandes diferencias con el programa anterior. Lo interesante es que acá se puede ver claramente cómo el valor de la influencia social K define el punto de equilibrio del sistema. Como la derivada es -x+K*(cosas que son casi 1), el sistema encuentra un punto de equilibrio en el valor x=K. Lo interesante entonces es que aumentar la cantidad de agentes modifica el múltiplo de K que sería el punto de convergencia.
    
    Esta versión tiene el RK4 mejorado que evoluciona de a una variable y es por tanto más generalizable que la versión anterior. Esta funciona iterando los sujetos desde afuera.
    También tiene implementadas las nuevas funciones de calcular Diferencias de vectores, Normas de vectores, una función de Iteración del sistema entero y agregué unos comandos en el main para escribir dos archivos de manera simultánea.
    
    De paso, en un momento cuando quise modificar esto el otro día me tiró errores de Segmentation Fault. No estoy seguro de haberlos corregido correctamente, pero juraría que el problema estaba en haber usado PRE incrementos en vez de POST incrementos. Así que por si acaso, le hacemos la cruz a los PRE incrementos de hoy en adelante, hasta que alguna experiencia trascendental me haga cambiar de opinión

---
+++ Programas C/Programas/Interacción de a pares/inicializar.h
@@ -0,0 +1,12 @@
+// Este es el header para el módulo general
+
+#ifndef Inicializar_H
+#define Inicializar_H
+#include <math.h>
+
+int GenerarOpi(s_Red var, s_Param par);
+int GenerarAng(s_Red var);
+int GenerarAdy(s_Red var, s_Param par);
+
+#endif
+
